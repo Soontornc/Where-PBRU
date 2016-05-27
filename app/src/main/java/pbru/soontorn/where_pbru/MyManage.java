3749a09825d982e54290e23e6adee8a2a11e1b26
Soontorn <soontornc008@gmail.com>
2016-05-27 12:26:09 +0700

    Success UI 1 + DB JSON Connect

--- app/src/main/java/pbru/soontorn/where_pbru/MyManage.java
+++ app/src/main/java/pbru/soontorn/where_pbru/MyManage.java
@@ -1,5 +1,6 @@
 package pbru.soontorn.where_pbru;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,12 +10,36 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class MyManage {
     private MyOpenHelper myOpenHelper;
-    private SQLiteDatabase sqLiteOpenHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
+    public static final String room_TABLE = "roomTABLE";
+    public static final String column_id = "_id";
+    public static final String column_Build = "Build";
+    public static final String column_Room = "Room";
+    public static final String column_Lat = "Lat";
+    public static final String column_Lng = "Lng";
+    public static final String column_Icon = "Icon";
 
     public MyManage(Context context) {
 
         myOpenHelper = new MyOpenHelper(context);
-        sqLiteOpenHelper = myOpenHelper.getWritableDatabase();
+        sqLiteDatabase = myOpenHelper.getWritableDatabase();
     }   //Constructor
+
+    public long addRoom(String strBuild,
+                        String strRoom,
+                        String strLat,
+                        String strLng,
+                        String strIcon) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_Build, strBuild);
+        contentValues.put(column_Room, strRoom);
+        contentValues.put(column_Lat, strLat);
+        contentValues.put(column_Lng, strLng);
+        contentValues.put(column_Icon, strIcon);
+
+        return sqLiteDatabase.insert(room_TABLE, null, contentValues);
+    }
 
 }   //Main Class
