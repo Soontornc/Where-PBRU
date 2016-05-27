69175eec2f2c943b53bbab80f7dcb4ba11abfdca
Soontorn <soontornc008@gmail.com>
2016-05-27 17:27:01 +0700

    Successfull Map Apps

--- app/src/main/java/pbru/soontorn/where_pbru/MapsActivity.java
+++ app/src/main/java/pbru/soontorn/where_pbru/MapsActivity.java
@@ -1,46 +1,169 @@
 package pbru.soontorn.where_pbru;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private String[] resultStrings;
+    private int[] iconInts = {R.drawable.build1, R.drawable.build2,
+            R.drawable.build3, R.drawable.build4,
+            R.drawable.build5, R.drawable.build6,};
+
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double myLatADouble, myLngADouble;
+    private boolean gpsABoolean, networkABoolean;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        resultStrings = getIntent().getStringArrayExtra("Result");
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }   //Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        locationManager.removeUpdates(locationListener);
+
+        myLatADouble = Double.parseDouble(resultStrings[3]);
+        myLngADouble = Double.parseDouble(resultStrings[4]);
+
+        Location ispLocation = myFindLocation(LocationManager.NETWORK_PROVIDER, "Not Connected Internet");
+
+        if (ispLocation != null) {
+
+            myLatADouble = ispLocation.getLatitude();
+            myLngADouble = ispLocation.getLongitude();
+
+        }
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER, "No Card GPS");
+
+        if (gpsLocation != null) {
+            myLatADouble = gpsLocation.getLatitude();
+            myLngADouble = gpsLocation.getLongitude();
+        }
+
+
     }
 
+    //ถ้าเกิดมีการขัดจังหวะขณะโปรแกรมทำงาน
+    @Override
+    protected void onStop() {
+        super.onStop();
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public Location myFindLocation(String strProvider,
+                                   String strError) {
+        Location location = null;
+        if (locationManager.isProviderEnabled(strProvider)) {
+            locationManager.requestLocationUpdates(strProvider,
+                    1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+        } else {
+            Log.d("whereV1", "My Error => " + strError);
+        }
+
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            myLatADouble = location.getLatitude();
+            myLngADouble = location.getLongitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Setup Center Map
+        setupCenterMap();
+
+        createMarkerUser();
+
+    }   //On Map Ready
+
+    private void createMarkerUser() {
+
+        LatLng latLng = new LatLng(myLatADouble, myLngADouble);
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.nobita48)));
+
     }
-}
+
+
+    private void setupCenterMap() {
+
+        double douLat = Double.parseDouble(resultStrings[3]);
+        double douLng = Double.parseDouble(resultStrings[4]);
+
+        LatLng latLng = new LatLng(douLat, douLng);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.fromResource(iconInts[Integer.parseInt(resultStrings[5])]))
+        .title("ห้อง "+resultStrings[2]).snippet("อยู่ที่อาคาร"+ resultStrings[1]));
+
+
+    }   //setup center
+
+}   //Main Class
